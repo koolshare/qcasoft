c4bfdc3ed0cb917e88fd4a9c04956e1e8d4366d7
sadoneli <sadoneli@gmail.com>
2020-09-25 11:28:27 +0800

    use qca as valid string

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ FOLDERS=$(ls -lh|grep -E "^d"|awk '{print $9}')
 
 for folder in ${FOLDERS}
 do
-	cd $folder
-	sh build.sh
-	cd ..
+	#cd $folder
+	#sh build.sh
+	#cd ..
+
+	cd ${folder}/${folder}
+	echo qca > .valid
+	cd ../..
 done
