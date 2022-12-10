bb0818567cfc1cb3f825f8ff329db159e13e7300
sadog <sadoneli@gmail.com>
2022-12-10 23:50:41 +0800

    update tailscale

--- tailscale/config.json.js
+++ tailscale/config.json.js
@@ -1,6 +1,6 @@
 {
 "version":"1.2",
-"md5":"ad52182c24d7739e40d7415072b79872",
+"md5":"4c02213e4c37bb60007c7b5be838c003",
 "home_url":"Module_tailscale.asp",
 "title":"Tailscale",
 "description":"基于wiregurad协议的零配置内网穿透安全组网工具！",
@@ -8,5 +8,5 @@
 "author":"sadog",
 "link":"",
 "changelog":"",
-"build_date":"2022-12-10_23:24:41"
+"build_date":"2022-12-10_23:50:29"
 }
